4561a687050a5009c16fa2664f720f87c42e2ce2
sultanby <jemjasott@gmail.com>
2022-06-07 15:24:49 +0200

    App state

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import React from "react";
+import React, {useState} from "react";
 import { Switch, Route, Redirect, NavLink } from "react-router-dom";
 
 import { AppointmentsPage } from "./containers/appointmentsPage/AppointmentsPage";
 import { ContactsPage } from "./containers/contactsPage/ContactsPage";
 
 function App() {
-  /*
-  Define state variables for 
-  contacts and appointments 
-  */
+  const [contacts, setContacts] = useState([]);
+  const [appointments, setAppointments] = useState([]);
 
   const ROUTES = {
     CONTACTS: "/contacts",
     APPOINTMENTS: "/appointments",
   };
 
-  /*
-  Implement functions to add data to
-  contacts and appointments
-  */
+  function handleContactSubmit(name, phone, mail) {
+    console.log('You clicked submit contact.');
+    
+    const newContact = {
+      name: name,
+      phone: phone,
+      mail: mail,
+    };
+
+    setContacts(prev => {
+      return {...prev, newContact}
+    });
+  }
+
+  function handleAppointmentSubmit(title, contact, date, time) {
+    console.log('You clicked submit appointment.');
+    
+    const newAppointment = {
+      title: title,
+      contact: contact,
+      date: date,
+      time: time,
+    };
+
+    setAppointments(prev => {
+      return {...prev, newAppointment}
+    });
+  }
 
   return (
     <>
@@ -36,12 +58,10 @@ function App() {
             <Redirect to={ROUTES.CONTACTS} />
           </Route>
           <Route path={ROUTES.CONTACTS}>
-             {/* Add props to ContactsPage */}
-            <ContactsPage />
+            <ContactsPage contacts={contacts} handleContactSubmit={handleContactSubmit}/>
           </Route>
           <Route path={ROUTES.APPOINTMENTS}>
-            {/* Add props to AppointmentsPage */}
-            <AppointmentsPage />
+            <AppointmentsPage appointments={appointments} handleAppointmentSubmit={handleAppointmentSubmit}/>
           </Route>
         </Switch>
       </main>
