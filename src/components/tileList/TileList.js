3c76bdf49ca76e9dfa9f63395e9da69cb21a5d52
sultanby <jemjasott@gmail.com>
2022-06-08 14:43:42 +0200

    TileList component done

--- src/components/tileList/TileList.js
+++ src/components/tileList/TileList.js
@@ -1,9 +1,12 @@
 import React from "react";
+import { Tile } from '../tile/Tile';
 
-export const TileList = () => {
+export const TileList = ({contacts}) => {
   return (
     <div>
-      
+      {contacts.map((contact, index) => {
+        <Tile contact={contact} key={index} />
+      })}
     </div>
   );
 };
